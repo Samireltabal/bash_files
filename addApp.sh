c3fd9fab7683afd90c192557176ea49d310a86c8
Samir Eltabbal <soly.0@live.com>
2018-03-17 15:35:56 +0200

    Update addApp.sh

--- addApp.sh
+++ addApp.sh
@@ -5,6 +5,7 @@ if [ ! -d /www/sites/$1/ ]
 	then
 		mkdir /www/sites/$1/
 		mkdir /www/sites/$1/wwwroot/
+		chmod -R 777 /www/sites/$1/wwwroot
 		echo " Account $1 Created "
 		echo " http://$1.local is App URL with Path /www/sites/$1/wwwroot "
 	else
